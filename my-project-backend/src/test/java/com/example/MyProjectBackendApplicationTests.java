1a49799ee06d70b398cdda77a83087183c609f6e
Myproject <1946695793@qq.com>
2026-08-04 22:09:19 +0800

    build: establish hermetic backend test baseline

--- my-project-backend/src/test/java/com/example/MyProjectBackendApplicationTests.java
+++ my-project-backend/src/test/java/com/example/MyProjectBackendApplicationTests.java
@@ -1,15 +1,20 @@
 package com.example;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-@SpringBootTest
+@SpringBootTest(
+		classes = MyProjectBackendApplicationTests.SmokeTestApplication.class,
+		webEnvironment = SpringBootTest.WebEnvironment.NONE
+)
 class MyProjectBackendApplicationTests {
 
 	@Test
-	void contextLoads() {
-		System.out.println(new BCryptPasswordEncoder().encode("123456"));
+	void contextLoadsWithoutExternalServices() {
 	}
 
+	@SpringBootConfiguration
+	static class SmokeTestApplication {
+	}
 }
